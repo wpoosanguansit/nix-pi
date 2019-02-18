783b6a20f145ffb562a6f19a29087aa6a6dda74a
Watt Poosanguansit <wpoosanguansit@yahoo.com>
2019-02-17 19:46:27 -0800

    commit user change

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix> ];
+  #imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-aarch64.nix> ];
   services.xserver = {
   	enable = true;
   	desktopManager.xfce.enable = true;
   	displayManager.auto.enable = true;
   	videoDrivers = [ "modsetting" ];
   };
-  networking.wireless.enable = true;
-  hardware.bluetooth.enable = true;
-  # NixOS wants to enable GRUB by default
-  boot.loader.grub.enable = false;
+  networking.hostId = "8425e340";
+
 
   # if you have a Raspberry Pi 2 or 3, pick this:
   boot.kernelPackages = pkgs.linuxPackages_latest;
@@ -24,21 +22,18 @@
   boot.loader.raspberryPi.firmwareConfig = ''
     gpu_mem=256
   '';
-  environment.systemPackages = with pkgs; [
-    raspberrypi-tools vim wpa_supplicant midori 
-  ];
 
   # File systems configuration for using the installer's partition layout
-  #fileSystems = {
-  #  "/boot" = {
-  #    device = "/dev/disk/by-label/NIXOS_BOOT";
-  #    fsType = "vfat";
-  #  };
-  #  "/" = {
-  #    device = "/dev/disk/by-label/NIXOS_SD";
-  #    fsType = "ext4";
-  #  };
-  #};
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_BOOT";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "zfs";
+    };
+  };
 
   # Preserve space by sacrificing documentation and history
   services.nixosManual.enable = false;
@@ -47,20 +42,34 @@
   boot.cleanTmpDir = true;
 
   # Configure basic SSH access
-  #services.openssh.enable = true;
-  #services.openssh.permitRootLogin = "yes";
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
 
   # Use 1GB of additional swap memory in order to not run out of memory
   # when installing lots of things while running other things at the same time.
   swapDevices = [ { device = "/swapfile"; size = 2048; } ];
 
+  networking.wireless.enable = true;
+  hardware.bluetooth.enable = true;
+  # NixOS wants to enable GRUB by default
+  boot.loader.grub.enable = false;
+
   users.extraUsers.admin = {
-	name = "admin";
-	group = "users";
-	extraGroups = [ "wheel" "disk" "video" "autdio" "networkmanager" "systemd-journal" ];
-	createHome = true;
-	uid = 1000;
-	home = "/home/admin";
+    name = "admin";
+    group = "users";
+    extraGroups = [ "wheel" "disk" "video" "autdio" "networkmanager" "systemd-journal" ];
+    createHome = true;
+    uid = 1000;
+    home = "/home/admin";
   };
+
+  environment.systemPackages = with pkgs; [
+    raspberrypi-tools vim wpa_supplicant midori 
+  ];
+
+  nixpkgs.config = { 
+    allowUnfree = true;
+    oraclejdk.accept_license = true;
+  }
 }
 
